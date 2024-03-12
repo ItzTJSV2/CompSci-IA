6af63e5d9ee8dcf5ed9c331a0fe48396435a39fc
ItzTJSV2 <maxtran7777@gmail.com>
2024-03-12 16:37:16 +0700

    k

--- MainProject/MainProject/Pages/Trips/CreateTrip.cshtml.cs
+++ MainProject/MainProject/Pages/Trips/CreateTrip.cshtml.cs
@@ -2,37 +2,32 @@ using Comp_Sci_IA_Main_Proj_.Pages.Accounts;
 using Comp_Sci_IA_Main_Proj_.Pages.Trips;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
 
 namespace MainProject.Pages.Trips
 {
     public class CreateTripModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
-        public string DestinationSubmit { get; set; }
+        public string? DestinationSubmit { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string OriginSubmit { get; set; }
+        public string? OriginSubmit { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string StopLatSubmit { get; set; }
+        public string? StopLatSubmit { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string StopLongSubmit { get; set; }
+        public string? StopLongSubmit { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string PeopleIDSubmit { get; set; }
+        public string? PeopleIDSubmit { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string TripNameSubmit { get; set; }
+        public string? TripNameSubmit { get; set; }
         public IActionResult OnGet()
         {
             if (User.Identity.Name != null)
             {
                 ViewData["Error"] = "";
-                ViewData["Destination"] = "";
-                ViewData["Origin"] = "";
-                ViewData["StopLat"] = "";
-                ViewData["StopLong"] = "";
-                ViewData["PeopleIDs"] = "";
-                ViewData["TripName"] = "";
                 return Page();
             }
             else
@@ -40,14 +35,37 @@ namespace MainProject.Pages.Trips
                 return RedirectToPage("/Index");
             }
         }
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPost()
         {
-            if (TripNameSubmit == "Testing Value")
+            string Destination = DestinationSubmit;
+            string Origin = OriginSubmit;
+            string TripName = TripNameSubmit;
+            string PeopleIDs;
+            List<string> PeopleIDArray = new List<string>();
+            List<string> StopLat = new List<string>();
+            List<string> StopLong = new List<string>();
+
+            TripDBInteract tripDBInteract = new TripDBInteract();
+            CheckDB database = new CheckDB();
+            List<string> AccountDetails = database.getAccount(User.Identity.Name);
+            string UserID = AccountDetails[0];
+
+            if (TripName == "")
             {
-                return RedirectToPage("MyTrips");
+                TripName = "Default Trip Name";
             }
-            return Page();
-             //return RedirectToPage("MyTrips");
+            if (PeopleIDSubmit == "")
+            {
+                PeopleIDs = UserID;
+            } else
+            {
+                PeopleIDs = $"{PeopleIDSubmit}, {UserID}";
+            }
+            for (int x = 0; x < StopLongSubmit.Count(c => c == ','); x++)
+            {
+
+            }
+            return RedirectToPage("MyTrips");
         }
 
     }
